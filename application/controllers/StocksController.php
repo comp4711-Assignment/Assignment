7f2532e779f2da1ccfc142f80c418703a08cb6e4
Cameron Stenmark <cameron.stenmark@iss-i.ca>
2016-02-14 14:38:57 -0800

    Created stock view and links
    -Cameron & John

--- application/controllers/StocksController.php
+++ application/controllers/StocksController.php
@@ -10,7 +10,20 @@ class StocksController extends Application {
     function display($name = null) {
         $this->data['pagebody'] = 'stock';
         $this->data['stockname'] = $name;
-        $this->data['stockdata'] = $this->stocks->get($name);
+        $row = $this->stocks->get($name);
+        
+        $list = $this->stocks->all();
+        
+        $dropdown = '';
+               
+        foreach($list as $item) {
+            $dropdown .= '<li><a href="'.$item->Code.'">'.$item->Name.'</a></li>';
+        }
+        
+        $this->data['stocklist'] = $dropdown;
+        
+        $this->data['stockdata'] = $row->Name;
+        
         $this->render();
     }
 }

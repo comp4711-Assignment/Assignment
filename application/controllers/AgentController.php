3982cafeb2b6b9fb4112aed897892dc07ed1a7e1
Cameron Stenmark <cameron.stenmark@iss-i.ca>
2016-04-14 15:40:35 -0700

    Added blank controller, model and view for Agent.  updated autoload for agent model
    -Cameron Stenmark

---
+++ application/controllers/AgentController.php
@@ -0,0 +1,20 @@
+<?php
+
+/************
+ * The agent controller that holds the functions to create the agent
+ * view class.
+ ******/
+class AgentController extends Application {
+    
+    /****
+     * Constructs the playercontroller.
+     */
+        var $equity = 0;
+        
+        function __construct()
+	{
+		parent::__construct();
+        }
+
+
+}
